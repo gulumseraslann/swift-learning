bd2d633d3d4d0fe3a86a884cc2606568881d3b6f
gulumser.aslan <gulumser.aslan@trendyol.com>
2021-04-25 11:28:46 +0300

    Initial Commit

---
+++ week2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  week2
+//
+//  Created by Gülümser Aslan on 25/04/2021.
+//  Copyright © 2021 Gülümser Aslan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
